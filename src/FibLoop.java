f9e53e79db5d62fc3f780996e9b197634026eb96
Diana Lewis <diana.lewis@vai.org>
2019-10-20 14:26:51 -0400

    Added fib number to output.

--- src/FibLoop.java
+++ src/FibLoop.java
@@ -15,6 +15,7 @@ public class FibLoop {
         static int MAXINPUTSIZE  = (int) Math.pow(1.5,28);
         static int MININPUTSIZE  =  1;
         static int Nums = 70;
+        static long fibResult = 0;
         // static int SIZEINCREMENT =  10000000; // not using this since we are doubling the size each time
 
         static String ResultsFolderPath = "/home/diana/Results/"; // pathname to results folder
@@ -31,11 +32,11 @@ public class FibLoop {
             // run the whole experiment at least twice, and expect to throw away the data from the earlier runs, before java has fully optimized
 
             System.out.println("Running first full experiment...");
-            runFullExperiment("FibRecur-Exp1-ThrowAway.txt");
+            runFullExperiment("FibLoop-Exp1-ThrowAway.txt");
             System.out.println("Running second full experiment...");
-            runFullExperiment("FibRecur-Exp2.txt");
+            runFullExperiment("FibLoop-Exp2.txt");
             System.out.println("Running third full experiment...");
-            runFullExperiment("FibRecur-Exp3.txt");
+            runFullExperiment("FibLoop-Exp3.txt");
         }
 
         static void runFullExperiment(String resultsFileName){
@@ -59,7 +60,7 @@ public class FibLoop {
             ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
 
             //add headers to text file
-            resultsWriter.println("#X(Value)    N(Size)    AverageTime    DoublingRatio"); // # marks a comment in gnuplot data
+            resultsWriter.println("#X(Value)  N(Size)  AverageTime    FibNumber"); // # marks a comment in gnuplot data
             resultsWriter.flush();
 
             /* for each size of input we want to test: in this case starting small and doubling the size each time */
@@ -101,7 +102,7 @@ public class FibLoop {
                     //TrialStopwatch.start(); // *** uncomment this line if timing trials individually
                     /* run the function we're testing on the trial input */
 
-                    long result = Fib(inputSize);
+                    fibResult = Fib(inputSize);
                     //System.out.println(result);
                     // batchElapsedTime = batchElapsedTime + TrialStopwatch.elapsedTime(); // *** uncomment this line if timing trials individually
                 }
@@ -119,7 +120,7 @@ public class FibLoop {
                 x++;
                 int countingbits = countBits(inputSize);
                 /* print data for this size of input */
-                resultsWriter.printf("%12d %12d %15.2f %15.2f \n",inputSize, countingbits, averageTimePerTrialInBatch, doublingTotal); // might as well make the columns look nice
+                resultsWriter.printf("%6d %6d %15.2f %20d \n",inputSize, countingbits, averageTimePerTrialInBatch, fibResult); // might as well make the columns look nice
                 resultsWriter.flush();
                 System.out.println(" ....done.");
             }
